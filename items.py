55b56e4397ac703146f81a206b90439c5ec586f5
Stefan Horst <stefan@ultrachaos.de>
2021-01-26 19:58:23 +0100

    move to format string; download public keys from server, if they are not in local repository; otherwise add public_key to metadata

--- items.py
+++ items.py
@@ -1,4 +1,5 @@
 from pipes import quote
+from bundlewrap.exceptions import NoSuchNode
 import socket
 
 RESTIC_VERSION = '0.9.6'
@@ -19,23 +20,22 @@ directories = {
 }
 
 downloads = {
-    '/opt/restic/restic_{version}.bz2'.format(version=RESTIC_VERSION): {
-        'url': 'https://github.com/restic/restic/releases/download/v{version}/restic_{version}_linux_amd64.bz2'.format(
-            version=RESTIC_VERSION,
-        ),
+    f'/opt/restic/restic_{RESTIC_VERSION}.bz2': {
+        'url': f'https://github.com/restic/restic/releases/download/v{RESTIC_VERSION}/restic_{RESTIC_VERSION}'
+                '_linux_amd64.bz2',
         'sha256': RESTIC_SHA256,
         'needs': ['directory:/opt/restic', 'pkg_apt:ca-certificates'],
         'triggers': ['action:unpack_restic'],
-        'unless': 'test -f /opt/restic/restic_{version}'.format(version=RESTIC_VERSION),
+        'unless': f'test -f /opt/restic/restic_{RESTIC_VERSION}',
     }
 }
 
 actions = {
     'unpack_restic': {
-        'command': 'bunzip2 -f /opt/restic/restic_{version}.bz2 '
-                   '&& chmod +x /opt/restic/restic_{version} '
-                   '&& rm -f /opt/restic/restic '  # remove old file
-                   '&& ln -s restic_{version} /opt/restic/restic'.format(version=RESTIC_VERSION),
+        'command': f'bunzip2 -f /opt/restic/restic_{RESTIC_VERSION}.bz2 '
+                    f'&& chmod +x /opt/restic/restic_{RESTIC_VERSION} '
+                     '&& rm -f /opt/restic/restic '  # remove old file
+                     f'&& ln -s restic_{RESTIC_VERSION} /opt/restic/restic',
         'needs': ['pkg_apt:bzip2'],
         'triggered': True,
     },
@@ -54,20 +54,31 @@ files = {
 
 
 for backup_host, backup_host_config in node.metadata.get('restic', {}).get('backup_hosts', {}).items():
-    identity_file = "~/.ssh/{host_name}".format(host_name=backup_host)
-    # TODO: make work without internet
-    backup_host_ip = socket.gethostbyname(backup_host)
-    port = backup_host_config.get('port', 22)
-    host_key = backup_host_config.get('hostkey')  # This should break, if it is not set!
+    try:
+        backup_node = repo.get_node(backup_host)
+        backup_host = backup_node.hostname
+        backup_host_ips = backup_node.metadata.get('interfaces', {}) \
+            .get(backup_node.metadata.get('main_interface', 'eth0'), {}) \
+            .get('ip_addresses', [])
 
-    actions['create_ssh_key_{host_name}'.format(host_name=backup_host)] = {
+        host_key = backup_node.metadata['openssh']['hostkey']  # This will break!!, if it is not set!
+    except NoSuchNode:
+        # TODO: make work without internet
+        backup_host_ips = [socket.gethostbyname(backup_host), ]
+        host_key = backup_host_config.get('hostkey')  # This should break, if it is not set!
+
+    identity_file = f"~/.ssh/{backup_host}"
+    port = backup_host_config.get('port', 22)
+
+    actions[f'create_ssh_key_{backup_host}'] = {
         'command': 'ssh-keygen -t ed25519 -f ~/.ssh/{host_name} -C {comment} -N ""'.format(
             host_name=backup_host,
             comment=quote(node.name)
         ),
-        'unless': 'test -f ~/.ssh/{host_name}'.format(host_name=backup_host),
+        'unless': f'test -f ~/.ssh/{backup_host}',
     }
-    actions['add_ssh_config_{host_name}'.format(host_name=backup_host)] = {
+    # node.download(f'/root/.ssh/{backup_host}.pub', 'test123')
+    actions[f'add_ssh_config_{backup_host}'] = {
         'command': 'echo "Host {host_name}\\n'
                    'user {backup_user}\\n'
                    'identityfile {identity_file}\\n'
@@ -78,15 +89,15 @@ for backup_host, backup_host_config in node.metadata.get('restic', {}).get('back
                         identity_file=identity_file,
                         port=port,
                    ),
-        'unless': 'grep -q \'{host_name}\' ~/.ssh/config'.format(host_name=backup_host),
+        'unless': f'grep -q \'{backup_host}\' ~/.ssh/config',
     }
 
     # add hostkey for hostname in known hosts file
     ssh_known_hosts_host_name = backup_host
     if port != 22:
-        ssh_known_hosts_host_name = "[{host_name}]:{port}".format(host_name=backup_host, port=port)
+        ssh_known_hosts_host_name = f"[{backup_host}]:{port}"
 
-    actions['add_known_host_{host_name}'.format(host_name=backup_host)] = {
+    actions[f'add_known_host_{backup_host}'] = {
         'command': 'echo {host_name} {host_key} >> ~/.ssh/known_hosts'.format(
             host_name=quote(ssh_known_hosts_host_name),
             host_key=quote(host_key)
@@ -97,21 +108,22 @@ for backup_host, backup_host_config in node.metadata.get('restic', {}).get('back
     }
 
     # add hostkey for ip in known hosts file
-    ssh_known_hosts_host_ip = backup_host_ip
-    if port != 22:
-        ssh_known_hosts_host_ip = "[{host_ip}]:{port}".format(host_ip=backup_host_ip, port=port)
+    for backup_host_ip in backup_host_ips:
+        ssh_known_hosts_host_ip = backup_host_ip
+        if port != 22:
+            ssh_known_hosts_host_ip = f"[{backup_host_ip}]:{port}"
 
-    actions['add_known_host_{host_ip}'.format(host_ip=backup_host_ip)] = {
-        'command': 'echo {host_ip} {host_key} >> ~/.ssh/known_hosts'.format(
-            host_ip=quote(ssh_known_hosts_host_ip),
-            host_key=quote(host_key)
-        ),
-        'unless': 'grep -q {host_ip} ~/.ssh/known_hosts'.format(
-            host_ip=quote(ssh_known_hosts_host_ip).replace('[', '\\[').replace(']', '\\]')
-        ),
-    }
+        actions[f'add_known_host_{backup_host_ip}'] = {
+            'command': 'echo {host_ip} {host_key} >> ~/.ssh/known_hosts'.format(
+                host_ip=quote(ssh_known_hosts_host_ip),
+                host_key=quote(host_key)
+            ),
+            'unless': 'grep -q {host_ip} ~/.ssh/known_hosts'.format(
+                host_ip=quote(ssh_known_hosts_host_ip).replace('[', '\\[').replace(']', '\\]')
+            ),
+        }
 
-    actions['print_ssh_key_{host_name}'.format(host_name=backup_host)] = {
+    actions[f'print_ssh_key_{backup_host}'] = {
         'command': 'echo "please register this ssh key on {host_name}:" && cat {identity_file}.pub && exit 255'.format(
             host_name=backup_host,
             identity_file=identity_file
@@ -122,21 +134,18 @@ for backup_host, backup_host_config in node.metadata.get('restic', {}).get('back
             'action:create_ssh_key_{host_name}'.format(host_name=backup_host),
             'action:add_ssh_config_{host_name}'.format(host_name=backup_host),
             'action:add_known_host_{host_name}'.format(host_name=backup_host),
-            'action:add_known_host_{host_ip}'.format(host_ip=backup_host_ip),
+            # 'action:add_known_host_{host_ip}'.format(host_ip=backup_host_ip),
         ]
     }
 
-    files['/etc/restic/password_{host_name}'.format(host_name=backup_host)] = {
-        'content': repo.vault.password_for("restic_password_{host_name}_{node_name}".format(
-            host_name=backup_host,
-            node_name=node.name
-        )),
+    files[f'/etc/restic/password_{backup_host}'] = {
+        'content': repo.vault.password_for(f"restic_password_{backup_host}_{node.name}").value,
         'owner': 'root',
         'group': 'root',
         'mode': '0600',
     }
 
-    actions['init_restic_{host_name}'.format(host_name=backup_host)] = {
+    actions[f'init_restic_{backup_host}'] = {
         'command': '/opt/restic/restic '
                    '--password-file /etc/restic/password_{host_name} '
                    '-r sftp://{host_name}/{node_name} '
@@ -146,15 +155,12 @@ for backup_host, backup_host_config in node.metadata.get('restic', {}).get('back
                    ),
         # we only allow rsync, sftp and scp
         # try to get config file, if it is not present, we will create the repository
-        'unless': 'rsync -n {host_name}:{node_name}/config /tmp || false'.format(
-                      host_name=backup_host,
-                      node_name=node.name,
-                   ),
+        'unless': f'rsync -n {backup_host}:{node.name}/config /tmp || false',
         'needs': [
-            'download:/opt/restic/restic_{version}.bz2'.format(version=RESTIC_VERSION),
-            'action:print_ssh_key_{host_name}'.format(host_name=backup_host),
+            f'download:/opt/restic/restic_{RESTIC_VERSION}.bz2',
+            f'action:print_ssh_key_{backup_host}',
             'action:unpack_restic',
-            'file:/etc/restic/password_{host_name}'.format(host_name=backup_host),
+            f'file:/etc/restic/password_{backup_host}',
         ],
     }
 
@@ -168,7 +174,7 @@ for backup_host, backup_host_config in node.metadata.get('restic', {}).get('back
             'pre_commands': node.metadata.get('restic', {}).get('pre_commands', []),
             'post_commands': node.metadata.get('restic', {}).get('post_commands', []),
             'stdin_commands': node.metadata.get('restic', {}).get('stdin_commands', {}),
-            'LOCK_FILE': '/tmp/restic_{host_name}.lock'.format(host_name=backup_host),
+            'LOCK_FILE': f'/tmp/restic_{backup_host}.lock',
             'RUN_HOUR': node.metadata.get('restic', {}).get('run_hour', 3),
         },
         'source': "cron_hourly.sh",
@@ -176,7 +182,7 @@ for backup_host, backup_host_config in node.metadata.get('restic', {}).get('back
         'group': 'root',
         'mode': '0755',
         'needs': [
-            'action:init_restic_{host_name}'.format(host_name=backup_host),
+            f'action:init_restic_{backup_host}',
         ]
     }
 
@@ -186,13 +192,13 @@ for backup_host, backup_host_config in node.metadata.get('restic', {}).get('back
             'backup_host': backup_host,
             'node_name': node.name,
             'keep': backup_host_config.get('keep', {}),
-            'LOCK_FILE': '/tmp/restic_{host_name}.lock'.format(host_name=backup_host),
+            'LOCK_FILE': f'/tmp/restic_{backup_host}.lock',
         },
         'source': "cron_daily.sh",
         'owner': 'root',
         'group': 'root',
         'mode': '0755',
         'needs': [
-            'action:init_restic_{host_name}'.format(host_name=backup_host),
+            f'action:init_restic_{backup_host}',
         ]
     }
--- metadata.py
+++ metadata.py
@@ -1,3 +1,7 @@
+from os.path import join, isfile
+from bundlewrap.utils import get_file_contents
+from bundlewrap.exceptions import NoSuchNode
+
 defaults = {}
 
 if node.has_bundle("apt"):
@@ -6,5 +10,33 @@ if node.has_bundle("apt"):
             'rsync': {'installed': True},
             'bzip2': {'installed': True},
             'ca-certificates': {'installed': True},
+        }
+    }
+
+
+@metadata_reactor
+def load_public_keys(metadata):
+    backup_hosts = {}
+    for backup_host, config in metadata.get('restic/backup_hosts', {}).items():
+        try:
+            backup_node = repo.get_node(backup_host)
+            backup_host = backup_node.hostname
+        except NoSuchNode:
+            pass
+
+        filename = join(repo.path, 'data', 'public_keys', f'{node.name}_{backup_host}.pub')
+
+        if not isfile(filename):
+            # download Node File
+            print('\n -- needed to download new file from host, consider adding it to GIT\n')
+            node.download(f'/root/.ssh/{backup_host}.pub', filename)
+
+        backup_hosts[backup_host] = {
+            'public_key': get_file_contents(filename),
+        }
+
+    return {
+        'restic': {
+            'backup_hosts': backup_hosts,
         }
     }
