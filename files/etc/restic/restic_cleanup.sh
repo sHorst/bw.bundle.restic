792118388460a59392c4b42f7f1706b46bc1a97a
DasLampe <daslampe@lano-crew.org>
2024-08-02 00:41:51 +0200

    fix: remove qoutes

--- files/etc/restic/restic_cleanup.sh
+++ files/etc/restic/restic_cleanup.sh
@@ -6,7 +6,7 @@ fi
 touch "$LOCK_FILE"
 
 # remove old files
-/opt/restic/restic forget -l "$KEEP_LAST" -H "$KEEP_HOURLY" -d "$KEEP_DAILY" -w "$KEEP_WEEKLY" -m "$KEEP_MONTHLY" -y "$KEEP_YEARLY" -q
+/opt/restic/restic forget -l $KEEP_LAST -H $KEEP_HOURLY -d $KEEP_DAILY -w $KEEP_WEEKLY -m $KEEP_MONTHLY -y $KEEP_YEARLY -q
 
 # purge old files
 /opt/restic/restic prune -q
